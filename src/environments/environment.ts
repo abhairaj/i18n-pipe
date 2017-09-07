6ca5aa2acad270fcea84c8c4edeaec31c490e6d7
Abhai Raj <abhairaj@jotisoftware.com>
2017-09-07 13:08:53 -0600

    dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 
 export const environment = {
   production: false,
-  rest: { baseUrl: 'localhost:3000/' },
-  twilio: {
-    baseUrl: 'https://api.twilio.com/2010-04-01/',
-    sid: 'AC4b0b0cbccd02c8e9d21ac501c77580f7',
-    authToken: 'f156d329a1d1a41de5036342cce7ce95',
-    from: '+15755876577 ',
-    defaultMediaUrl: 'http://haciendayoga.org/img/yb9-gr.jpg'
-  },
   i18nPipe: {
     urlBase: 'assets/i18n/',
     defaultLang: 'en'
